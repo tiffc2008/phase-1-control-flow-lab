77cdc59e595c0f68968a80f90d5c25c9f0ae9eaf
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-18 13:00:12 -0600

    commit tree d062728970a35384395c3bcd13a220a7a3eb3464

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance)
+{
+  if (distance < 400) {
+    return `This one is on me!`
+  }
+  else if (distance > 400 && distance < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }  
+  else if (distance > 2500){
+    return 'No can do.'
+  }
+  }
+
+function ternaryCheckCity(city){
+   return (city === "NYC" ? "Ok, sounds good." : 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  console.log(tip)
+  let response;
+  switch (tip) {
+  case 'generous' :
+    response = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    response = 'Thank you.';
+    break;
+  default:
+    response = 'Bye.'
+  }
+  return response
 }
